3791258bfd340917bb5a4c51f8cb403f6b3e51c1
ManlyMarco <dummyaccx@gmail.com>
2020-06-04 00:07:40 +0200

    Fixed some late created canvases not being found in ai

--- src/HideAllUI.AISyoujyo/HideMakerUI.cs
+++ src/HideAllUI.AISyoujyo/HideMakerUI.cs
@@ -9,14 +9,14 @@ namespace HideAllUI
         private IEnumerable<Canvas> canvasList;
         private bool visible = true;
 
-        public HideMakerUI()
-        {
-            var go = GameObject.Find("CustomControl");
-            canvasList = go.GetComponentsInChildren<Canvas>().Where(x => x.gameObject.name.Contains("Canvas"));
-        }
-
         public override void ToggleUI()
         {
+            if(canvasList == null)
+            {
+                var go = GameObject.Find("CustomControl");
+                canvasList = go.GetComponentsInChildren<Canvas>().Where(x => x.gameObject.name.Contains("Canvas"));
+            }
+
             visible = !visible;
             foreach(var canvas in canvasList.Where(x => x))
                 canvas.enabled = visible;
